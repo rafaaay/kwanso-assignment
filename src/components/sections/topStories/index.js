f1276c83f6991acf2754c721916dafaeef3f545e
rafay <rafay.ed13@gmail.com>
2023-10-20 22:37:31 +0500

    feat: json mapped for story sections

--- src/components/sections/topStories/index.js
+++ src/components/sections/topStories/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import SectionTitle from '../../atoms/sectionTitle';
 import StoryCard from '../../atoms/storyCard';
 
+import TopStoriesRecord from "../../../top-stories.json";
+
 function TopStories() {
   return (
     <section className='section topStories'>
@@ -20,37 +22,19 @@ function TopStories() {
 
             {/* Top Stories */}
             <div className='grid'>
-              <StoryCard 
-                  title="Here Goes a really long title"
-                  author="Author Name"
-                  date="20 August, 2022"
-                  topic="Technology"
-                  layout="vertical"
-              />
-
-              <StoryCard 
-                  title="Here Goes a really long title"
-                  author="Author Name"
-                  date="20 August, 2022"
-                  topic="Technology"
-                  layout="vertical"
-              />
-
-              <StoryCard 
-                  title="Here Goes a really long title"
-                  author="Author Name"
-                  date="20 August, 2022"
-                  topic="Technology"
-                  layout="vertical"
-              />
-
-              <StoryCard 
-                  title="Here Goes a really long title"
-                  author="Author Name"
-                  date="20 August, 2022"
-                  topic="Technology"
-                  layout="vertical"
-              />
+            {
+                TopStoriesRecord.map( record => {
+                    return(
+                        <StoryCard 
+                            title={record.title}
+                            author={record.author}
+                            date={record.date}
+                            topic={record.topic}
+                            layout="vertical"
+                        />
+                    )
+                })
+            }
             </div>
             
             {/* Top Stories */}
--- src/components/sections/trendingStories/index.js
+++ src/components/sections/trendingStories/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SectionTitle from '../../atoms/sectionTitle';
 import StoryCard from '../../atoms/storyCard';
+import TrendingStoriesRecord from "../../../trending-stories.json";
 
 function TrendingStories() {
   return (
@@ -9,29 +10,19 @@ function TrendingStories() {
             <SectionTitle title="Trending Stories" />
 
             <div className='grid'>
-              <StoryCard 
-                  title="Here Goes a really long title"
-                  author="Author Name"
-                  date="20 August, 2022"
-                  topic="Technology"
-                  layout="horizontal"
-              />
-
-              <StoryCard 
-                  title="Here Goes a really long title"
-                  author="Author Name"
-                  date="20 August, 2022"
-                  topic="Technology"
-                  layout="horizontal"
-              />
-
-              <StoryCard 
-                  title="Here Goes a really long title"
-                  author="Author Name"
-                  date="20 August, 2022"
-                  topic="Technology"
-                  layout="horizontal"
-              />
+            {
+                TrendingStoriesRecord.map( record => {
+                    return(
+                        <StoryCard 
+                            title={record.title}
+                            author={record.author}
+                            date={record.date}
+                            topic={record.topic}
+                            layout="vertical"
+                        />
+                    )
+                })
+            }
             </div>
         </div>
     </section>
